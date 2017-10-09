08c1f667eaf2596f27534f25f862a559c4d986df
Pau Fernández <pau.fernandez@upc.edu>
2017-10-09 15:10:33 +0200

    Començo WebConnect

---
+++ app/src/main/java/edu/upc/eseiaat/pma/webconnect/WebConnectActivity.java
@@ -0,0 +1,13 @@
+package edu.upc.eseiaat.pma.webconnect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WebConnectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_connect);
+    }
+}
